ceff1a576bceecd28869fdcbba60857bdaa4b49e
Graeson Bergen <graesonbergen@gmail.com>
2022-11-15 00:13:43 -0700

    imports from glsl working using vite plugin

--- src/components/MainCanvas.tsx
+++ src/components/MainCanvas.tsx
@@ -1,49 +1,19 @@
 import { Canvas } from '@react-three/fiber';
 import { useState, useEffect } from 'react';
-
-const vertex: string = `
-varying vec3 vNormal;
-
-void main() {	
-    vec3 localPosition = vec3(position);
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(localPosition, 1.0);
-
-    vNormal = (modelMatrix * vec4(normal, 0.0)).xyz; // normals in world space
-  
-}
-`;
-
-const fragment: string = `
-varying vec3 vNormal;
-
-void main() {
-    gl_FragColor = vec4(vNormal, 1.0);
-}
-`;
+import vertexShader from '../shaders/main_sphere/vertex.glsl';
+import fragmentShader from '../shaders/main_sphere/fragment.glsl';
 
 export default function MainCanvas() {
-  const [vertexShader, setVertexShader] = useState('void main() {}');
-  const [fragmentShader, setFragmentShader] = useState('void main() {}');
-
-  useEffect(()=> {
-    async function setShaders() {
-      const vsh = await fetch('./shaders/vertex.glsl');
-      setVertexShader(await vsh.text());
-      const fsh = await fetch('./shaders/fragment.glsl');
-      setFragmentShader(await fsh.text());
-    } 
-
-  }, [])
 
   return(
     <div className="border-red-700 w-screen h-screen border absolute">
       <Canvas>
         <mesh>
-          <icosahedronGeometry args={[2,50]} />
+          <icosahedronGeometry args={[2,20]} />
           <shaderMaterial args={[{
             uniforms: {},
-            vertexShader: vertex,
-            fragmentShader: fragment
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader
           }]} />
         </mesh>
       </Canvas>
--- src/shaders/main_sphere/fragment.glsl
+++ src/shaders/main_sphere/fragment.glsl
@@ -5,37 +5,6 @@ vec3 rgbToFloat(vec3 color) {
 }
 
 void main() {
-    vec3 sphereColour = rgbToFloat(vec3(20.0, 30.0, 40.0));
-    vec3 viewDir = normalize(cameraPosition - vPosition);
-    // vec3 normal = normalize(vNormal);
-    vec3 normal = normalize(
-        cross(
-            dFdx(vNormal.xyz),
-            dFdy(vNormal.xyz)
-        )
-    );
-
-    // lighting
-    float hemiDot = clamp(0.0, 1.0, dot(up, normal));
-    hemi = pow(hemiDot, 16.0) * hemi;
-    float diffuseDot = max(0.0, dot(normal, -diffuseDir));
-    diffuseDot *= smoothstep(0.1, 0.105, diffuseDot);
-    float threeShades = mix(0.7, 1.0, step(0.4, diffuseDot)) * step(0.105, diffuseDot);
-    diffuse = diffuseDot * diffuse * threeShades;
-
-    vec3 reflection = normalize(reflect(-diffuseDir, normal));
-    float specDot = max(0.0, dot(reflection, -viewDir));
-    vec3 specular = vec3(pow(specDot, 64.0));
-    //specular *= vec3(step(0.5, specular.x));
-
-    float fresnelDot = 1.0 - max(0.0, dot(viewDir, normal));
-    
-    vec3 fresnel = vec3(pow(fresnelDot, fresnelMod));
-    fresnel *= rgbToFloat(vec3(210.0, 210.0, 190.0));
-
-    vec3 colour = fresnel + sphereColour;
-
-    //colour = pow(lighting, vec3(1.0 / 2.2));
 
     gl_FragColor = vec4(vNormal, 1.0);
 }
