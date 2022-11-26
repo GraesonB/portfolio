fd1f7234ecac655ff22acd6d46c59b0a7dd916cb
Graeson Bergen <graesonbergen@gmail.com>
2022-11-26 14:04:02 -0700

    better responsiveness

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,20 @@
+import { Canvas } from '@react-three/fiber';
+import MainSphere from './MainSphere'
+
 type ButtonProps = {
   text: String
 }
 
-export default function Button(props: ButtonProps) {
+export default function Button({ text }: ButtonProps) {
+
   return (
-    <h3 className='text-center hover:text-[#8FABA8] font-thin text-[#CDF5F0] mb-10 mt-10'>{props.text}</h3>
+    <div className="flex justify-center items-center m-8 p-2 w-64">
+      <div className="w-1/2 h-20 absolute mix-blend-difference rounded-xl">
+        {/* <Canvas camera={{position: [0,0,30], fov: 10}}>
+          <MainSphere desiredRotation={[0,0,0]} mousePos={[0,0]} position={[0,0,0]} radius={1.4}  />
+        </Canvas> */}
+      </div>
+        <a className='block hover:text-[#8FABA8] font-thin text-[#CDF5F0] text-5xl z-10 top-0 pb-3 mix-blend-difference'>{text}</a>
+    </div>
   );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,13 +4,13 @@ import Button from './Button'
 
 export default function SideBar() {
   return (
-    <div className='flex flex-col justify-center text-5xl absolute xl:4/5 h-full self-end invisible lg:visible'>
+    <div className='flex flex-col justify-center absolute xl:4/5 h-full self-end invisible lg:visible'>
       <div className="w-full h-full absolute mix-blend-difference">
         {/* <Canvas camera={{position: [0,0,50], fov: 10}}>
           <MainSphere desiredRotation={[0,0,0]} mousePos={[0,0]} position={[5,0,0]} radius={8}  />
         </Canvas> */}
       </div>
-      <ul className='w-full h-full p-16'>
+      <ul className='flex flex-col justify-center items-center w-full h-full p-16 top-0 z-10'>
         <Button text={'projects'} />
         <Button text={'contact'} />
         <Button text={'resume'} />
