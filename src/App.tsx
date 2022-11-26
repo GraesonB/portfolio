319a558cd95c434270a02225533b9e0c650a366a
Graeson Bergen <graesonbergen@gmail.com>
2022-11-26 16:26:41 -0700

    sphere and name move together now

--- src/App.tsx
+++ src/App.tsx
@@ -21,16 +21,15 @@ export default function App() {
   return (
 
     <div className="flex flex-col justify-center items-center w-full h-screen">
-      <div className="w-[24rem] h-[24rem] sm:w-[32rem] sm:h-[32rem] lg:w-[32rem] lg:h-[32rem] 2xl:w-[48rem] 2xl:h-[48rem] absolute mix-blend-difference">
-        <Canvas camera={{position: [0,0,50], fov: 10}}>
-          <MainSphere desiredRotation={[0,0,0]} mousePos={mousePos} position={[0,0,0]} radius={3}  />
-        </Canvas>
-      </div>
       <div className="flex justify-center items-center text-[2.5rem] sm:text-6xl lg:text-6xl 2xl:text-8xl w-full md:w-11/12 lg:w-4/5 h-full flex-col flex-wrap">
+        <div className="w-[24rem] h-[24rem] sm:w-[32rem] sm:h-[32rem] lg:w-[32rem] lg:h-[32rem] 2xl:w-[48rem] 2xl:h-[48rem] absolute mix-blend-difference">
+          <Canvas camera={{position: [0,0,50], fov: 10}}>
+            <MainSphere desiredRotation={[0,0,0]} mousePos={mousePos} position={[0,0,0]} radius={3}  />
+          </Canvas>
+        </div>
         <h1 className='text-center font-thin text-[#CDF5F0]'>GRAESON<b>BERGEN</b></h1>
         <h1 className="text-xl sm:text-4xl lg:text-4xl 2xl:text-5xl font-thin text-[#CDF5F0] mt-1 sm:mt-0">full stack developer</h1>
       </div>
-    
       <SideBar />
     </div>
   )
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,13 +8,8 @@ type ButtonProps = {
 export default function Button({ text }: ButtonProps) {
 
   return (
-    <div className="flex justify-center items-center m-8 p-2 w-64">
-      <div className="w-1/2 h-20 absolute mix-blend-difference rounded-xl">
-        {/* <Canvas camera={{position: [0,0,30], fov: 10}}>
-          <MainSphere desiredRotation={[0,0,0]} mousePos={[0,0]} position={[0,0,0]} radius={1.4}  />
-        </Canvas> */}
-      </div>
-        <a className='block hover:text-[#8FABA8] font-thin text-[#CDF5F0] text-5xl z-10 top-0 pb-3 mix-blend-difference'>{text}</a>
+    <div className="flex justify-center items-center m-8 p-2 w-64 border-[#CDF5F0]">
+        <a className='block hover:text-[#8FABA8] font-thin text-[#CDF5F0] text-5xl pb-3'>{text}</a>
     </div>
   );
 }
