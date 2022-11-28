4bf1c81b156025a76f80bc076bc137aa456f0064
Graeson Bergen <graesonbergen@gmail.com>
2022-11-27 22:44:53 -0700

    naming

--- src/components/MainSphere/MainSphere.tsx
+++ src/components/MainSphere/MainSphere.tsx
@@ -5,23 +5,23 @@ import MainSphereMesh from "./MainSphereMesh";
 import elemCenter from "../../helpers/elem-center"
 
 export default function MainSphere() {
-  const sphereCanvasRef = useRef(null);
-  const [sphereCanvasCenter, setSphereCanvasCenter] = useState<[number, number]>([0,0]);
+  const sphereRef = useRef(null);
+  const [center, setCenter] = useState<[number, number]>([0,0]);
 
 
   useEffect(() => {
-    if (sphereCanvasRef.current !== null) setSphereCanvasCenter(elemCenter(sphereCanvasRef.current));
-  }, [sphereCanvasRef]);
+    if (sphereRef.current !== null) setCenter(elemCenter(sphereRef.current));
+  }, [sphereRef]);
 
   const resize = () => {
-    if (sphereCanvasRef.current !== null) setSphereCanvasCenter(elemCenter(sphereCanvasRef.current));
+    if (sphereRef.current !== null) setCenter(elemCenter(sphereRef.current));
   } 
   window.addEventListener('resize', resize);
 
   return (
-    <div ref={sphereCanvasRef} className='w-full h-full'>
+    <div ref={sphereRef} className='w-full h-full'>
       <Canvas camera={{position: [0,0,50], fov: 10}}>
-        <MainSphereMesh canvasPos={sphereCanvasCenter} desiredRotation={[0,0,0]} position={[0,0,0]} radius={3}  />
+        <MainSphereMesh canvasPos={center} desiredRotation={[0,0,0]} position={[0,0,0]} radius={3}  />
       </Canvas>
     </div>
   );
